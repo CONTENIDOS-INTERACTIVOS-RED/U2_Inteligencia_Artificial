64fad389797c17ebb603679a7350c0977fcfaddb
Francisco Lizcano <flizcanor@icloud.com>
2025-06-02 19:18:47 -0500

    se ajusta sintesis

--- src/views/curso/Sintesis.vue
+++ src/views/curso/Sintesis.vue
@@ -2,7 +2,7 @@
   .curso-main-container.creditos-vista
     BannerInterno(subTitulo="SÍNTESIS")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-      p(data-aos="fade-up").mb-5 La Unidad 3: Bioética y sostenibilidad en el desarrollo económico global, establece los principios esenciales para comprender cómo los valores éticos y bioéticos pueden orientar el desarrollo económico hacia la sostenibilidad. Esta unidad proporciona una perspectiva integral sobre la relación entre la economía, el medio ambiente y los derechos humanos, destacando la importancia de prácticas responsables en contextos globalizados. Este enfoque asegura que los estudiantes adquieran una comprensión sólida de los desafíos éticos actuales y las estrategias sostenibles, permitiéndoles aplicar estas herramientas de manera efectiva en su formación profesional y en la vida cotidiana.
+      p(data-aos="fade-up").mb-5 La unidad 2: Principios del aprendizaje automático, introduce los fundamentos teóricos y prácticos del Machine Learning, permitiendo comprender cómo las computadoras pueden aprender de los datos para hacer predicciones o tomar decisiones. Esta unidad proporciona una base sólida sobre los tipos de aprendizaje (supervisado y no supervisado), los algoritmos básicos de regresión y clasificación, y sus aplicaciones en problemas cotidianos. Este enfoque integral asegura que los estudiantes adquieran habilidades esenciales para aplicar técnicas de aprendizaje automático en contextos reales, lo que les permitirá avanzar con éxito en el análisis de datos y el desarrollo de soluciones inteligentes.
 
       .row.justify-content-center
         .col-lg-12.mb-5
